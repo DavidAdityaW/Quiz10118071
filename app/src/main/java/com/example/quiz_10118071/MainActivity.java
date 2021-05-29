e4899c495acfa1ffd2240712dc499f8b1e02bb2d
DavidAdityaW <david.10118071@mahasiswa.unikom.ac.id>
2021-05-29 10:57:53 +0700

    tampilan aplikasi DigiTeam - 10118071 David Aditya W

---
+++ app/src/main/java/com/example/quiz_10118071/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.quiz_10118071;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    Dikerjakan pada tanggal : 29 Mei 2021
+    Deskripsi pekerjaan : Membuat layouting tampilan pada aplikasi DigiTeam, implementasi custom design pada tampilan
+    Dibuat oleh :
+    NIM   : 10118071
+    Nama  : David Aditya Winarto
+    Kelas : IF-2
+*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
